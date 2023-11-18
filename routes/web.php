a2f2bd8bf3acdefbc1bd60265e25673f3928816a
muhammadayaan2001 <muhammad.ayan2977@gmail.com>
2023-11-18 10:42:39 +0500

    - chat contorller and view

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\User\ChatController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +22,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/chats', [ChatController::class, 'index'])->name('chat-index');
